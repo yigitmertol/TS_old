2a8fd77d45e7af8f74d3254665c63e211630d5b1
Unknown <yigitmertolkayabasi@gmail.com>
2018-06-14 18:05:05 +0200

    Moving towards classification/ Fixing regression issues
    Many issues in preprocessing handled
    TODO: handle NaN and 0/inf/-inf  values in original raw data properly.
    
    Trend guessing script initiated.

--- FF_SnP_regression.py
+++ FF_SnP_regression.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 import numpy as np
 
 class Network:
-    I_WINDOW = 20
+    I_WINDOW = 10
     INPUTS_DIM = 0
     O_WINDOW = 1
 
-    def __init__(self, threads, seed=42):
+    def __init__(self, threads, input_dim, seed=42):
         # Create an empty graph and a session
         graph = tf.Graph()
         graph.seed = seed
-        self.INPUTS_DIM = len(args.feats.split('-'))
+        self.INPUTS_DIM = input_dim
         self.session = tf.Session(graph = graph, config=tf.ConfigProto(inter_op_parallelism_threads=threads,
                                                                        intra_op_parallelism_threads=threads))
 
@@ -24,11 +24,11 @@ class Network:
 
             # FOR NORMAL FF NETWORK WE JUST FLATTEN ALL INPUTS ALONG THE INPUT DIM
             features = tf.layers.flatten(self.x)
-            hidden_layer = tf.layers.dense(features, 256, activation=tf.nn.relu, name="hidden_layer")
+            hidden_layer = features
+            hidden_layer = tf.layers.dense(hidden_layer, 512, activation=tf.nn.relu, name="hidden_layer")
             hidden_layer = tf.layers.dense(hidden_layer, 256, activation=tf.nn.relu, name="hidden_layer2")
-            hidden_layer = tf.layers.dense(hidden_layer, 128, activation=tf.nn.relu, name="hidden_layer3")
-            # hidden_layer = tf.layers.dense(hidden_layer, 64, activation=tf.nn.sigmoid, name="hidden_layer4")
-            hidden_layer = tf.layers.dense(hidden_layer, 32, activation=tf.nn.relu, name="hidden_layer5")
+            hidden_layer = tf.layers.dense(hidden_layer, 32, activation=tf.nn.relu, name="hidden_layer3")
+            # hidden_layer = tf.layers.dense(hidden_layer, 32, activation=tf.nn.relu, name="hidden_layer5")
             # hidden_layer = tf.layers.dense(hidden_layer, 256, activation=tf.nn.relu, name="hidden_layer6")
             #
             output_layer = tf.layers.dense(hidden_layer, self.O_WINDOW, activation=None, name="output_layer")
@@ -37,7 +37,7 @@ class Network:
             # Training
             # loss = tf.losses.mean_squared_error(self.y, output_layer, scope="loss")
 
-            loss = tf.losses.absolute_difference(self.y, output_layer, scope='loss')
+            loss = tf.losses.mean_squared_error(self.y, output_layer, scope='loss')
             self.loss =loss
             global_step = tf.train.create_global_step()
             self.training = tf.train.AdamOptimizer().minimize(loss, global_step=global_step, name="training")
@@ -85,14 +85,28 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=5, type=int, help="Batch size.")
-    parser.add_argument("--horizon", default=10, type=int, help="how far in t after last observation we try to predict")
+
+    # Training Parameters Epoch Batch Thread
+    parser.add_argument("--batch_size", default=10, type=int, help="Batch size.")
     parser.add_argument("--epochs", default=1000, type=int, help="Number of epochs.")
-    parser.add_argument("--hidden_layer", default=50, type=int, help="Size of the hidden layer.")
     parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+
+    # Architecture of the network
+    parser.add_argument("--arch", default="", type=str, help="Size of the hidden layer.")
+
+    # Task parameters(input window length and how far the predictions are)
+    parser.add_argument("--horizon", default=1, type=int, help="how far in t after last observation we try to predict")
     parser.add_argument("--window_len", default=Network.I_WINDOW, type=int, help="Window length of input")
-    parser.add_argument("--market", default='AAL', type=str, help="Window length of input")
-    parser.add_argument("--feats", default='close-open-high-low', type=str, help="Window length of input")
+
+    # parser.add_argument("--markets", default='AAL-', type=str, help="Window length of input")
+    parser.add_argument("--target", default='ABT-volatiliy', type=str, help="The predicted value")
+
+
+    parser.add_argument("--inputs", default='target-all,normal_close,normal_high', type=str,
+                        help="features that will be loaded fromeach market data set")
+
+    # First feature is at the same time the regressed one
+
     # parser.add_argument("--a", default=10, type=int, help="Window length of input")
 
     args = parser.parse_args()
@@ -105,29 +119,52 @@ if __name__ == "__main__":
     )
     if not os.path.exists("logs"): os.mkdir("logs") # TF 1.6 will do this by itself
 
-    # # Load the data
-    # from tensorflow.examples.tutorials import mnist
-    # mnist = mnist.input_data.read_data_sets("mnist_data/", reshape=False, seed=42)
+
+    # Create the processor object and get the data
     import preprocessing_SNP
     prcs = preprocessing_SNP.processor()
     prcs.init(args.batch_size, args.window_len, args.horizon)
-    prcs.load_SNP_processed(args.market)
-    # prcs.make_feats(save_file=True)
-    feats = args.feats.split('-')
-    prcs.select_feats(['t'] + feats, 'close')
+    target = args.target.split('-')
+    prcs.load_SNP_processed(target[0])
+    prcs.select_n_add_to_Y(target[1])
 
+    inputs = args.inputs.split('-')
+
+    for input in inputs:
+        if input == "target":
+            prcs.Target_is_feat = True
+            prcs.select_n_add_to_X([target[1]])
+            continue
+        if input.split(',')[0] == "all":
+            for market_file_name in os.listdir('./SnP_processed'):
+                market = market_file_name.split('_')[2]
+                if market == target[1]:
+                    continue
+                prcs.load_SNP_processed(market)
+                feats = input.split(',')[1:]
+                prcs.select_n_add_to_X(feats)
+        else:
+            market = input.split(',')[0]
+            prcs.load_SNP_processed(market)
+            feats = input.split(',')[1:]
+            prcs.select_n_add_to_X(feats)
+
+
+    prcs.handle_Inf()
     prcs.split_Train_dev_test(_slide=1)
-    prcs.make_sliding_wind_samples(feats,  _slide=1)
+    # prcs.normalize()
+    prcs.make_sliding_wind_samples(_slide=1)
 
-    prcs.shuffle_samples()
+    # prcs.shuffle_samples()
+    print("Variance of Y(dev set): " + str(round(np.var(prcs.Y_dev), 3)))
     print("Same as last baseline rmse: " + str(prcs.get_baseline_("same")))
-    print("Linear model baseline rmse: " + str(prcs.get_baseline_("linear")))
+    # print("Linear model baseline rmse: " + str(prcs.get_baseline_("linear")))
 
 
 
 
     # Construct the network
-    network = Network(threads=args.threads)
+    network = Network(threads=args.threads, input_dim=len(prcs.X.columns))
     network.construct(args)
 
 
@@ -137,13 +174,13 @@ if __name__ == "__main__":
         epoch_finished=False
         prcs.shuffle_samples()
 
-        if i % 10 == 0:
-            rmse_tr, pred_ = network.evaluate("train", prcs.X_tr, prcs.Y_tr)
-            rmse, pred= network.evaluate("dev", prcs.X_dev, prcs.Y_dev)
+        if i % 2 == 0:
+            rmse_tr, pred_ = network.evaluate("train", prcs.X_w_tr, prcs.Y_w_tr)
+            rmse, pred= network.evaluate("dev", prcs.X_w_dev, prcs.Y_w_dev)
 
-            ind = np.random.randint(len(prcs.X_dev) - 5)
-            print("some real values: " + str([round(y[0], 2) for y in prcs.Y_dev[ind:ind + 5]]))
-            print("predictions were: " + str([round(y[0], 2) for y in pred[ind:ind + 5]]))
+            ind = np.random.randint(len(prcs.Y_w_dev) - 10)
+            print("some real values: " + str([round(y[0], 2) for y in prcs.Y_w_dev[ind:ind + 10]]))
+            print("predictions were: " + str([round(y[0], 2) for y in pred[ind:ind + 10]]))
 
             print("Dev rmse: " + str(round(rmse, 4)))
             print("Tr rmse: " + str(round(rmse_tr, 4)))
--- preprocessing_SNP.py
+++ preprocessing_SNP.py
@@ -15,6 +15,7 @@ class processor:
     Target = ''
     Input_window_length = 0
     Forecast_horizon = 0
+    Target_is_feat = False
 
     def init(self, batch_size = 0, _input_window_l = 0, _horizon = 0):
 
@@ -22,15 +23,15 @@ class processor:
         self.Forecast_horizon = _horizon
 
         # all data is the co collection of all time series and tables
-        self.raw_data = None
+        self.Data_loaded = None
         self.batchsize = batch_size
 
         # all data with new devised features
         self.with_feats = None
 
         # Selected features and predicted column
-        self.X = []
-        self.Y = []
+        self.X = pd.DataFrame()
+        self.Y = pd.DataFrame()
 
         # Training samples in format like:
         # X = X[][x(t) for t in INPUT TIME INTERVAL]
@@ -53,37 +54,47 @@ class processor:
         self.Y_te = []
         self.Y_w_te = []
 
-
-
-    # some financial feature engineering from
-    # Deep Learning Stock Volatility with Google Domestic Trends
-    # Ruoxuan Xiong1, Eric P. Nichols2 and Yuan Shen
-
     def u(self):
-        X = self.X
-        return math.log(X['high'] / X['open'])
-
+        X = self.Data_loaded
+        return np.log(np.array(X['high']) / np.array(X['open']))
     def d(self):
-        X =  self.X
-        return math.log(X['low'] / X['open'])
-
+        X =  self.Data_loaded
+        return np.log(np.array(X['low']) / np.array(X['open']))
+    def c(self):
+        X = self.Data_loaded
+        return np.log(np.array(X['close']) / np.array(X['open']))
 
     # loading data
     def load_SNP(self, market):
         self.Market = market
         if market == "All":
-            self.raw_data = pd.read_csv("sandp500/all_stocks_5yr.csv")
+            self.Data_loaded = pd.read_csv("sandp500/all_stocks_5yr.csv")
         else:
-            self.raw_data = pd.read_csv("sandp500/individual_stocks_5yr/" + str(market) + "_data.csv")
+            self.Data_loaded = pd.read_csv("sandp500/individual_stocks_5yr/" + str(market) + "_data.csv")
 
     def load_SNP_processed(self, market):
         self.Market = market
-        self.with_feats = pd.read_csv('./SnP_processed/SnP_market_' + str(self.Market) + "_with_features.csv")
+        self.Data_loaded = pd.read_csv('./SnP_processed/SnP_market_' + str(self.Market) + "_with_features.csv")
 
 
+        # This function used once to create files in SnP processed no need until fresh clone
 
+    def process_markets(self):
+        for file_name in os.listdir("sandp500/individual_stocks_5yr"):
+            if '.DS_' in file_name:
+                continue
+            market = file_name.split('_')[0]
+            self.load_SNP(market)
+            # self.select_n_add_to_X()
+            self.make_feats(True)
+
+
+    def handle_column_with_nans(self, col):
+        for i in range(len(col)):
+            if col[i]
     def make_feats(self, save_file):
-        data = self.raw_data
+        data = self.Data_loaded
+
         # Add t as the time variable
         data['t'] = range(len(data))
 
@@ -94,59 +105,97 @@ class processor:
         data['day_of_month'] = [d.split('-')[2] for d in date]
         data['day_of_week'] = data['t'] % 7
 
-        data['weekend'] = [float(int(x)) for x in np.array(data['day_of_week'] == 1).__or__(np.array(data['day_of_week'] == 2))]
-        cl = data['close']
+
+        # boolean is it a weekend
+        data['weekend'] = [int(x) for x in np.array(data['day_of_week'] == 1).__or__(np.array(data['day_of_week'] == 2))]
+
         # returns is at t is the log difference close prices of t to t-1
-        data['return'] = [float(1) for row in range(len(data))]
-        data['normal_close'] = cl
+        data['return'] = np.zeros_like(len(data['t']))
+        data['return'][0] = 1
 
-        for ind in range(len(data['normal_close'])):
-            data['normal_close'][ind] = data['normal_close'][ind] / data['open'][0]
 
-        for t in range(1, len(data)):
-            data['return'][t] = ((cl[t] / cl[t - 1])-1) * 100
+        normal = data['open'][0]
+
+
+        data['normal_close'] = list(np.array(data['close']) / normal)
+        data['normal_open'] = list(np.array(data['open']) / normal)
+        data['normal_high'] = list(np.array(data['high']) / normal)
+        data['normal_low'] =  list(np.array(data['low']) / normal)
+        data['return'][1:] = list(np.array(data['close'][1:] - np.array(data['close'][:-1])))
+
+        # some financial feature engineering from
+        # Deep Learning Stock Volatility with Google Domestic Trends
+        # Ruoxuan Xiong1, Eric P. Nichols2 and Yuan Shen
+        # the above paper references to an older paper on financial
+        # volatiliy where this formula is devised
+        u = np.array(self.u())
+        d = np.array(self.d())
+        c = np.array(self.c())
+        data['volatility'] = 0.511*np.square(u-d) + 0.019*(c*( u+d)-2*u*d) - 0.383*np.square(c)
 
         self.with_feats = data
         if save_file:
             self.with_feats.to_csv('./SnP_processed/SnP_market_' + str(self.Market) + "_with_features.csv")
 
-
-    def select_feats(self, _feats, _target):
-        self.Target = _target
-        if _feats == None:
-            self.X = self.with_feats
-        else :
-            self.X = self.with_feats[_feats]
-
+    def select_n_add_to_Y(self,  _target = None):
         if _target == None:
-            self.Y = self.with_feats
+            self.Target = _target
+            self.Y = None
+            print("Target is selected None ==> Y is empty(None)")
         else:
-            self.Y = self.with_feats[_target]
+            self.Target = _target
+            self.Y = self.Data_loaded[_target]
+            print("Regressed(predicted/targeted) variable(s) is " + str(_target) + " of "+ self.Market)
 
+    def select_n_add_to_X(self,  _feats = None):
 
-    # slides over t in X and generates input output time series
-    # Slided windows are subsets of Original self.X Data Frame ==>  they preserve structure as:
-    # first dim: features(with names of columns), second is time index(index in original data frame)
-    # This way until you cast it to an array you will always have the real time index
-    def make_sliding_wind_samples(self, _in_column, _slide, _sets = 'all'):
+        data = self.Data_loaded
+
+        for feat in _feats:
+            if feat == None:
+                for col in data.columns:
+                    self.X[str(self.Market) + "_" + str(col)] = data[col]
+            elif feat == "4normals":
+                for col in ['normal_high', 'normal_low', 'normal_open', 'normal_close']:
+                    self.X[str(self.Market) + "_" + str(col)] = data[col]
+            else:
+                self.X[str(self.Market) + "_" + str(feat)] = data[feat]
+
+    def handle_Inf(self):
+        self.X[~np.isfinite(self.X)] = 0
+
+    def make_sliding_wind_samples(self, _slide =1, _trend = False,  _sets = 'all'):
+
+        # slides over t in X and generates input output time series
+        # Slided windows are subsets of Original self.X Data Frame ==>  they preserve structure as:
+        # first dim: features(with names of columns), second is time index(index in original data frame)
+        # This way until you cast it to an array you will always have the real time index
 
         sets = dict()
         if _sets == 'all':
+            for set in ['tr', 'dev', 'te']:
+                sets[set] = dict()
+
             sets['tr'] = [self.X_tr, self.Y_tr, self.X_w_tr, self.Y_w_tr]
             sets['dev'] = [self.X_dev, self.Y_dev, self.X_w_dev, self.Y_w_dev]
             sets['te'] = [self.X_te, self.Y_te, self.X_w_te, self.Y_w_te]
 
-
-        t_0 = 0
         w = self.Input_window_length
         h = self.Forecast_horizon
 
         for set in sets:
-            while (t_0 + w + h < len(sets[set][0]['t'])):
-                x = np.array(sets[set][0][_in_column][t_0: t_0 + w])
-                y = np.array([sets[set][1][t_0 + w + h]])
+            T_0 = sets[set][0].index[0]
+            t_0 = 0
+            T_1 = sets[set][0].index[-1]
+            while (T_0 + w + h <= T_1):
+                x = np.array(sets[set][0][:][t_0:t_0 + w])
+                y = np.array([sets[set][1][T_0 + w + h-1]])
+                trend = 1 if x[-1][0] < y[0] else 0
                 t_0 += _slide
+                T_0 += _slide
                 sets[set][2].append(x)
+                if _trend:
+                    y = trend
                 sets[set][3].append(y)
 
 
@@ -155,19 +204,17 @@ class processor:
 
         self.maxn_full_batches = int(len(self.X_tr) / self.batchsize)
 
-
-    def preprocess_markets(self):
-        for file_name in os.listdir("sandp500/individual_stocks_5yr")[1:]:
-            market = file_name.split('_')[0]
-            self.load_SNP(market)
-            self.make_feats(True)
-
-
     def normalize(self):
-        self.X[self.X.columns.drop(['return', 't'])] = preprocessing.normalize(self.X[self.X.columns.drop(['return', 't'])])
+        df = self.X_tr
+        self.X_tr = (df - df.mean()) / df.std()
 
-    # TODO: Decide(find out) if the output of training set can intersect with inout of validation/test
-    # divide the time series or divide samples
+        df = self.X_dev
+        self.X_dev = (df - df.mean()) / df.std()
+
+        df = self.X_te
+        self.X_te = (df - df.mean()) / df.std()
+
+
     def split_Train_dev_test(self, _slide, _divide ='temporal', _perc_of_test = 20, _perc_of_dev = 20):
 
         h = self.Forecast_horizon
@@ -203,7 +250,6 @@ class processor:
             self.X_tr = self.X[:dev_ind_begin]
             self.Y_tr = self.Y[:dev_ind_begin]
 
-
     def flatten_Xs(self, data):
         flats = []
         for x in data:
@@ -213,13 +259,12 @@ class processor:
             flats.append(flat)
         return flats
 
-
     def shuffle_samples(self):
-        combined = list(zip(self.X_tr, self.Y_tr))
+        combined = list(zip(self.X_w_tr, self.Y_w_tr))
         shuffle(combined)
-        self.X_tr[:], self.Y_tr[:] = zip(*combined)
+        self.X_w_tr[:], self.Y_w_tr[:] = zip(*combined)
 
-
+    # TODO Check get next batch
     # def get_next_batch(self):
     #
     #     N = self.maxn_full_batches
@@ -237,25 +282,33 @@ class processor:
     #                self.Y_tr[n*self.batchsize:(n+1)*self.batchsize],\
     #                False
 
+    # def consolidate(self, feats):
+    #
 
+    # TODO Check baseline functions are indeed correctly functioning
     def get_baseline_(self, _baseline):
 
         # Baseline prediction where prediction = last observation(assuming observation
         if _baseline == 'same':
+
+            if not self.Target_is_feat:
+                print("The target feat is not contained in regressor(predictors)")
+                return
             pred = []
-            for i in range(len(self.Y_dev)):
+            for i in range(len(self.X_w_dev)):
                 # Conversion to NP array here no more
                 # FINAL DECISION MADE: CONVERT TO MATRIX FROM DATA FRAME IN SLIDING WINDOWS
-                x=np.array(self.X_dev[i][self.Input_window_length-1])
-
-                # Hence zero is always target(regressed observation)
+                x=np.array(self.X_w_dev[i][self.Input_window_length-1])
+                # Hence zero (after sample index and time index within the sample
+                # (these are actually overlapping except on time index))
+                # is always target(regressed observation)
                 pred.append(x[0])
-            return np.sqrt(mean_squared_error(pred, np.array(self.Y_dev)))
+            return np.sqrt(mean_squared_error(pred, np.array(self.Y_w_dev)))
 
         elif _baseline == "linear":
             lm = linear_model.LinearRegression()
-            lm.fit(self.flatten_Xs(self.X_tr), self.Y_tr)
-            return np.sqrt(mean_squared_error(lm.predict(self.flatten_Xs(self.X_dev)), self.Y_dev))
+            lm.fit(self.flatten_Xs(self.X_w_tr), self.Y_w_tr)
+            return np.sqrt(mean_squared_error(lm.predict(self.flatten_Xs(self.X_w_dev)), self.Y_w_dev))
 
 
         elif _baseline == "vol_wei_mean":
@@ -266,11 +319,3 @@ class processor:
         # # volume weighted average of window
         # if baseline == "vol_we_ave":
 
-
-
-# print()
-
-
-# prcs = processor()
-# prcs.init(10,10,10)
-# prcs.preprocess_markets()
